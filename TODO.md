3c3e8768f5882cfaa4bae0be5e6ff282ae859dfc
Sakshi Jagtap <99532147+sakshikjagtap@users.noreply.github.com>
2022-07-05 11:18:26 +0530

    Extracted Puzzle entity.
    * Changed logic of validating moves.

---
+++ TODO.md
@@ -0,0 +1,10 @@
+- ## TODO
+
+  - [ ] add empty div instead of white image
+  - [ ] Decide group size based on number of tiles.
+ 
+- ## DONE
+
+  - [x] modelled data
+  - [x] Rethink about problem 
+  - [x] Rename images
--- jigsawsPuzzle.js
+++ jigsawsPuzzle.js
@@ -1,76 +1,162 @@
-(() => {
-  const images = [
-    "image_part_001.jpg", "image_part_002.jpg", "image_part_003.jpg", "image_part_004.jpg", "image_part_005.jpg", "image_part_006.jpg", "image_part_007.jpg", "image_part_008.jpg", "white.jpg"
-  ];
+const retrieveImage = (id) => {
+  const srcImgPath = document.getElementById(id).src;
+  return srcImgPath.split('/').slice(-1);
+};
 
-  const generateRandomPosition = () => {
+const getValidMoves = ([row, col]) => {
+  return [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]];
+};
+
+class Puzzle {
+  #grid;
+  constructor(imageTiles, blankTile) {
+    this.imageTiles = imageTiles;
+    this.blankTile = blankTile;
+    this.#grid = [];
+  }
+
+  initialize() {
+    this.#grid = shuffleImages([...this.imageTiles, this.blankTile]);
+  }
+
+  #getTilePosition(tile) {
     const positions = [];
 
-    while (positions.length < 9) {
-      const randomNumber = Math.floor(Math.random() * 9);
-
-      if (!positions.includes(randomNumber)) {
-        positions.push(randomNumber);
+    this.#grid.forEach((cell, index) => {
+      if (cell.includes(tile)) {
+        positions.push(index, cell.indexOf(tile));
       }
-    }
+    });
     return positions;
   }
 
-  const shuffleImages = (images) => {
-    const positions = generateRandomPosition();
-    return positions.map((position) => images[position]);
-  };
+  isValidMove([srcImgRow, srcImgCol]) {
+    const desImgPosition = this.#getTilePosition(this.blankTile);
+    const validMoves = getValidMoves(desImgPosition);
 
-  const generateImageHtml = (images) => {
-    return images.map((image, position) => {
+    return validMoves.some(([row, col]) =>
+      row === srcImgRow && col === srcImgCol);
+  }
+
+  moveTile(id) {
+    const [srcImg] = retrieveImage(id);
+    const [srcImgRow, srcImgCol] = this.#getTilePosition(srcImg);
+    const [desImgRow, desImgCol] = this.#getTilePosition(this.blankTile);
+
+    if (this.isValidMove([srcImgRow, srcImgCol])) {
+      const desImg = this.#grid[desImgRow][desImgCol];
+      this.#grid[desImgRow][desImgCol] = srcImg;
+      this.#grid[srcImgRow][srcImgCol] = desImg;
+    }
+  }
+
+  isSolved() {
+    const tiles = [...this.imageTiles, this.blankTile];
+    const shuffledTiles = this.#grid.flat();
+    return tiles.every((image, index) => image === shuffledTiles[index]);
+  }
+
+  getGrid() {
+    return this.#grid;
+  }
+}
+
+const generateRandomPosition = () => {
+  const positions = [];
+
+  while (positions.length < 9) {
+    const randomNumber = Math.floor(Math.random() * 9);
+
+    if (!positions.includes(randomNumber)) {
+      positions.push(randomNumber);
+    }
+  }
+  return positions;
+}
+
+const groupTiles = (shuffledTiles) => {
+  const grid = [];
+  let count = 0;
+
+  while (shuffledTiles.length > count) {
+    grid.push(shuffledTiles.slice(count, count + 3));
+    count += 3;
+  }
+  return grid;
+};
+
+const shuffleImages = (images) => {
+  const positions = generateRandomPosition();
+  const shuffledTiles = positions.map(position => images[position]);
+  return groupTiles(shuffledTiles);
+};
+
+const generateImageHtml = (images) => {
+  let position = 1;
+
+  return images.map(cell => {
+    return cell.map(image => {
       const currentImage = document.createElement('img');
       currentImage.src = 'images/' + image;
-      currentImage.id = position + 1;
+      currentImage.id = position;
+      position++;
       return currentImage;
     });
+  });
+};
+
+const generateRows = (count) => {
+  return Array(count).fill(0).map((ele, index) => {
+    const row = document.createElement('div');
+    row.className = 'row';
+    row.id = 'row-' + (index + 1);
+    return row;
+  });
+};
+
+const generatePuzzleHtml = (images) => {
+  const imageHtml = generateImageHtml(images);
+  const rows = generateRows(images.length);
+  let count = 0;
+
+  for (const row of rows) {
+    const imgElements = imageHtml[count];
+    row.append(...imgElements);
+    count += 1;
+  }
+  return rows;
+};
+
+const displayMessage = () => {
+  const message = document.getElementById('message');
+  message.innerText = 'Congratulation, you solved it..!';
+};
+
+const generateNumbers = (count, start = 0, step = 1) => (Array(count).fill(' ')).map((x, i) => start + i * step);
+
+const draw = (grid) => {
+  const frame = document.getElementById('puzzle-frame');
+  frame.innerHTML = null;
+  const frameElements = generatePuzzleHtml(grid);
+  frame.append(...frameElements);
+};
+
+const startPuzzle = (event, puzzle) => {
+  puzzle.moveTile(event.target.id);
+  draw(puzzle.getGrid());
+
+  if (puzzle.isSolved()) {
+    displayMessage();
   };
+};
 
-  const generatePuzzleHtml = (images) => {
-    const imageHtml = generateImageHtml(images);
-    const rows = document.getElementsByClassName('row');
-    let count = 0;
+const main = () => {
+  const puzzle = new Puzzle(generateNumbers(8, 1).map(n => `${n}.jpg`), 'white.jpg');
+  puzzle.initialize();
+  draw(puzzle.getGrid());
+  document.addEventListener('click', (event) => startPuzzle(event, puzzle));
+}
 
-    for (const row of rows) {
-      const imgElements = imageHtml.slice(count, count + 3);
-      row.append(...imgElements);
-      count += 3;
-    }
-  };
+window.onload = main;
 
-  const swap = (shuffledImages, srcImgId, desImgId) => {
-    const srcImg = shuffledImages[srcImgId - 1];
-    const desImg = shuffledImages[desImgId - 1];
-    document.getElementById(desImgId).src = `images/${srcImg}`;
-    document.getElementById(srcImgId).src = `images/${desImg}`;
-    shuffledImages[srcImgId - 1] = desImg;
-    shuffledImages[desImgId - 1] = srcImg;
-  };
 
-  const isValidMove = (srcImgId, desImgId) => {
-    const diff = desImgId - srcImgId;
-    const possibilities = [1, -1, 3, -3];
-    return possibilities.includes(diff);
-  };
-
-  const startPuzzle = (event, shuffledImages) => {
-    let srcImgId = event.target.id;
-    let desImgId = shuffledImages.indexOf('white.jpg') + 1;
-
-    if (!isValidMove(srcImgId, desImgId)) {
-      return;
-    }
-    swap(shuffledImages, srcImgId, desImgId);
-    desImgId = srcImgId;
-  };
-
-  window.onload = () => {
-    const shuffledImages = shuffleImages(images);
-    generatePuzzleHtml(shuffledImages);
-    document.addEventListener('click', (event) => startPuzzle(event, shuffledImages));
-  };
-})();
